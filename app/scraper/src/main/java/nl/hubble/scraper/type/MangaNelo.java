16408518daa3fc76a49500262038d0c0c292a17f
Quentin Correia <481246@student.saxion.nl>
2021-05-27 17:02:04 +0200

    Added manganato and fixed mangafast also changed main menu item order and icons

--- app/scraper/src/main/java/nl/hubble/scraper/type/MangaNelo.java
+++ app/scraper/src/main/java/nl/hubble/scraper/type/MangaNelo.java
@@ -14,6 +14,6 @@ public class MangaNelo extends QueryScraper {
 
     @Override
     public String[] hostnames() {
-        return new String[]{"manganelo.com", "m.mangabat.com"};
+        return new String[]{"manganelo.com", "m.mangabat.com", "readmanganato.com", "manganato.com"};
     }
 }
